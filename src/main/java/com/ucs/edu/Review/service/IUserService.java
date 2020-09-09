97196b747b55f54bb7455c12bf8d8cfa9098a387
Minko <minminko22696@gmail.com>
2020-09-08 21:15:16 -1200

    UPDATED

---
+++ src/main/java/com/ucs/edu/Review/service/IUserService.java
@@ -0,0 +1,13 @@
+package com.ucs.edu.Review.service;
+
+import java.util.List;
+
+import com.trustcore.security.dto.LoginUserDTO;
+import com.trustcore.security.model.UserRole;
+
+public interface IUserService {
+	
+	public void saveUser(LoginUserDTO user);
+	public List<UserRole> getUserRoles();
+
+}
